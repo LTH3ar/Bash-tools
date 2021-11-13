3786ff71898934e0a31fb8eb28080477c36e8e93
LTH3ar <92171829+LTH3ar@users.noreply.github.com>
2021-11-13 11:07:55 +0700

    Update cal.cpp

--- cal.cpp
+++ cal.cpp
@@ -17,30 +17,33 @@ int main(){
             case 1:
                 cout << "System is on"<< endl << "Calculator start" << endl;
                 
-                float V0, Theta, g, d, t, h;
-                bool choice1;
-
+                float V0;
                 cout << "Initial velocity: ";
                 cin >> V0;
 
+                float Theta;
                 cout << "Angle: ";
                 cin >> Theta;
 
+                float g;
                 g = 10;
 
+                float h;
                 cout << "Initial height: ";
                 cin >> h;
 
+                float d;
                 d = (V0 * cos(Theta * M_PI *pow(180,-1)) / g) * (V0 * sin(Theta* M_PI *pow(180,-1)) + sqrt(pow(V0, 2)*(pow(sin(Theta * M_PI * pow(180,-1)), 2)) + (2 * g * h)));
 
+                float t;
                 t = 2 * V0 * sin(Theta* M_PI * pow(180,-1));
 
                 cout << "Horizontal range: " << d << endl << "Time: " << t << endl;
 
                 cout << "Do you want to do it again?" << endl << "1 for yes or 0 for no" << endl;
                 
+                bool choice1;
                 cin >> choice1;
-
                 if (choice1 == true){
                     continue;
                 }
